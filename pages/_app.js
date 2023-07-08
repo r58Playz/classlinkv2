2c5aaf156cac50cd908c2c1d8b5f4d8141694bff
r58Playz <r58playz@gmail.com>
2023-07-08 14:47:00 +0530

    nprogress dropped

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import 'nprogress/nprogress.css';
 import '@/styles/catppuccin.css';
+import '@/styles/theming.css';
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { themeList } from '@/components/themes.js';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+NProgress.configure({
+    minimum: 0.3,
+    easing: "ease",
+    speed: 800,
+    showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => {NProgress.start()})
+Router.events.on('routeChangeComplete', () => {NProgress.done()})
+Router.events.on('routeChangeError', () => {NProgress.done()})
 
 export default function App({ Component, pageProps }) {
   return (
