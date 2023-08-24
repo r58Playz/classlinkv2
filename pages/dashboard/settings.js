9ad43ede7ba4a6c7025a85571f504c77e36668e6
r58Playz <r58playz@gmail.com>
2023-08-24 15:28:42 -0700

    themebutton :skull:

--- pages/dashboard/settings.js
+++ pages/dashboard/settings.js
@@ -2,12 +2,14 @@ import Classlinkv2Layout from '@/components/dashboard/layout.js';
 import styles from '@/styles/profile.module.css';
 import { uiHelper } from '@/lib/classlink.js';
 import Settings from '@/components/dashboard/settings.js';
+import ThemeButton from '@/components/themes.js';
 
 export default function SettingsPage({sd}) {
   return (
     <Classlinkv2Layout title="Settings">
       <div className={styles.heading}>Settings</div>
       <Settings />
+      <ThemeButton />
     </Classlinkv2Layout>
   )
 }
